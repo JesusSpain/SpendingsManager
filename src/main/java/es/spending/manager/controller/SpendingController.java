3fe37473725b239576813b85f1dfea6974cc1676
JesusSpain <jesustrujillo5023@gmail.com>
2021-05-29 19:14:15 +0200

    Branch_CreatingSpendingServicesAndRestController. Fixing error at resource type not found in getAllBasicSpending.

--- src/main/java/es/spending/manager/controller/SpendingController.java
+++ src/main/java/es/spending/manager/controller/SpendingController.java
@@ -3,6 +3,7 @@ package es.spending.manager.controller;
 import es.spending.manager.exception.ResourceNotFoundException;
 import es.spending.manager.spending.beans.PeriodicalSpendingBean;
 import es.spending.manager.spending.beans.SpendingBean;
+import es.spending.manager.spending.model.BasicSpending;
 import es.spending.manager.spending.model.PeriodicalSpending;
 import es.spending.manager.spending.service.BasicSpendingService;
 import es.spending.manager.spending.service.PeriodicalSpendingService;
@@ -42,7 +43,7 @@ public class SpendingController {
         if (CollectionUtils.isEmpty(spendings)) {
             throw new ResourceNotFoundException(
                     messageSource.getMessage(MessagesKeys.MSG_RESOURCE_NOT_FOUND, null, locale),
-                    PeriodicalSpending.class.getCanonicalName());
+                    BasicSpending.class.getCanonicalName());
         }
         return spendings;
     }
